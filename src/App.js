9f386015a8e5d88f57a58838f6e8cec32ed38ebb
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-03 15:10:48 +0300

    Create different sections

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 function App() {
   return (
     <>
+    <nav>Navbar</nav>
+    <section>
+      <div>Booking</div>
+      <div>Table</div>
+    </section>
+
     
     </>
   );
