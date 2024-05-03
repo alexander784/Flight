5778ce28c2997e7c6a090146692bac37804773fe
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-03 15:12:16 +0300

    Create a Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
-    <nav>Navbar</nav>
+    <Header />
     <section>
       <div>Booking</div>
       <div>Table</div>
