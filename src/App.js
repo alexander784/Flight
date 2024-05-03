be3d0066dd13a400552f0c9e5d87c28a137642ac
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-03 18:09:54 +0300

    Design background image

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 import Header from './components/Header';
+// import beach from "../src/assets/beach.jpeg";
 
 function App() {
   return (
-    <>
+    <div className='bg-image bg-cover bg-no-repeat'>
     <Header />
-    <section>
+    <section className='h-screen'>
       <div>Booking</div>
       <div>Table</div>
     </section>
 
     
-    </>
+    </div>
   );
 }
 
