27d75ec532b7d93b95dfde9d20532137db8a174d
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-03 15:24:43 +0300

    Done Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
-
+import { MdOutlineFlightTakeoff } from "react-icons/md";
 
 const Header = () => {
   return (
-    <header className="w-full px-4 bg-slate-50">
+    <header className="w-full p-4 bg-slate-50">
         <nav className="flex items-center justify-between max-w-6x1 mx-auto">
-            <a href="/" className="text-lg font-bold">Travel Logo</a>
+            <a href="/" className="text-lg font-bold flex items-center"><MdOutlineFlightTakeoff className="text-2xl mr-1 text-indigo-600"/>Travel Logo</a>
             <button className="bg-indigo-600 text-whitepx-6 py-2 rounded font-medium ">Login</button>
         </nav>
     </header>
