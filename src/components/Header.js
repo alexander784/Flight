f16cddae3014633075944ccf5b1ef9b8c13ba3c4
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-03 15:18:54 +0300

    Structure Header section

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,12 @@
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="w-full px-4 bg-slate-50">
+        <nav className="flex items-center justify-between max-w-6x1 mx-auto">
+            <a href="/" className="text-lg font-bold">Travel Logo</a>
+            <button className="bg-indigo-600 text-whitepx-6 py-2 rounded font-medium ">Login</button>
+        </nav>
+    </header>
   )
 }
 
