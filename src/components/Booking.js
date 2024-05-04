bc485132f4bb41a7d9f89411d85405b59dd14f7a
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-04 08:21:17 +0300

    Handle booking and bookings selection

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 
 const Booking = () => {
+    const [bookingData, setbookingData] = useState({});
+
+    const handleChange = (e) => {
+        // console.log(e.target.value)
+        const newBookingData = {...bookingData};
+        newBookingData[e.target.name] = e.target.value;
+        setbookingData(newBookingData);
+
+
+    }
+    const handleBook = (e) => {
+        e.preventDefault();
+        console.log(e.target.value);
+    }
+    console.log(bookingData);
+
   return (
-    <div className="mt-[160px] mx-4 relative">
+    <div className="md:mt-[160px] mt-[100px] mx-4 relative">
         <div className="bg-white rounded-md max-w-6xl w-full mx-auto">
             <form className="flex flex-col md:flex-row">
                 <div className="py-1.5 px-2.5 flex-1 border-r-2">
-                    <p>Destination form</p>
+                    <p>Destination from</p>
                     <div className="flex flex-row">
-                        <select name="from" id="from" className="outline-none p-2 w-full">
+                        <select required onChange={(e) => handleChange(e)} name="from" id="from" 
+                        className="outline-none p-2 w-full">
                             <option value="">Please select</option>
                             <option>Nairobi</option>
                             <option>Kampala</option>
@@ -20,7 +38,7 @@ const Booking = () => {
                 <div className="py-1.5 px-2.5 flex-1 border-r-2">
                     <p>Destination To</p>
                     <div className="flex flex-row">
-                        <select name="from" id="from" className="outline-none p-2 w-full">
+                        <select name="to" id="to" onChange={(e) => handleChange(e)} className="outline-none p-2 w-full">
                             <option value="">Please select</option>
                             <option>Nairobi</option>
                             <option>Kampala</option>
@@ -31,7 +49,8 @@ const Booking = () => {
                         {/* date */}
                         <div className="py-1.5 px-2.5 flex-1 border-r-2">
                     <p>Travel Date</p>
-                    <input type="date" name="date" required className="outline-none p-2 w-full"/>
+                    <input type="date" name="date" required onChange={(e) => handleChange(e)}
+                    className="outline-none p-2 w-full"/>
                 </div>
 
 
@@ -41,7 +60,8 @@ const Booking = () => {
                 <div className="py-1.5 px-2.5 flex-1 border-r-2">
                     <p>Guest</p>
                     <div className="flex flex-row">
-                        <select name="from" id="from" className="outline-none p-2 w-full">
+                        <select name="guests" id="guests" onChange={(e) => handleChange(e) }
+                         className="outline-none p-2 w-full">
                             <option value="">Please select</option>
                             <option>1 person</option>
                             <option>2 persons</option>
@@ -54,7 +74,8 @@ const Booking = () => {
                 <div className="py-1.5 px-2.5 flex-1 border-r-2">
                     <p>Travel Class</p>
                     <div className="flex flex-row">
-                        <select name="from" id="from" className="outline-none p-2 w-full">
+                        <select name="ticketclassName" id="from" onChange={(e) => handleChange(e)}
+                         className="outline-none p-2 w-full">
                             <option value="">Please select</option>
                             <option>Busines</option>
                             <option>Economy</option>
@@ -63,7 +84,7 @@ const Booking = () => {
                     </div>
                 </div>
                 {/* button */}
-                <button type="submit" className="bg-indigo-500 text-white px-8 py-1 space-x-2 text-center flex items-center justify-center"><FaPlus className="mr-1" />Book Now</button>
+                <button onClick={(e)=> handleBook(e)}type="submit" className="bg-indigo-500 text-white px-8 py-1 space-x-2 text-center flex items-center justify-center"><FaPlus className="mr-1" />Book Now</button>
 
             </form>
         </div>
