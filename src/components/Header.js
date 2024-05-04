8bc3ae9b330578bf537e0f5a461ef33b592bbbf4
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-04 10:36:25 +0300

    Customize logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="w-full p-4 bg-slate-50">
         <nav className="flex items-center justify-between max-w-6x1 mx-auto">
-            <a href="/" className="text-lg font-bold flex items-center"><MdOutlineFlightTakeoff className="text-2xl mr-1 text-indigo-600"/>Travel Logo</a>
+            <a href="/" className="text-lg font-bold flex items-center"><MdOutlineFlightTakeoff className="text-2xl mr-1 text-indigo-600"/>Lexa Travels</a>
             <button className="bg-indigo-600 text-whitepx-6 py-2 rounded font-medium ">Login</button>
         </nav>
     </header>
